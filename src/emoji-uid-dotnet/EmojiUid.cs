257dfab956a5dc3b2fbbd1965f76b1af7d1f9925
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-08-04 14:50:18 +0100

    ft: add in length validation

--- src/emoji-uid-dotnet/EmojiUid.cs
+++ src/emoji-uid-dotnet/EmojiUid.cs
@@ -10,6 +10,9 @@ namespace emoji_dotnet
 
         public static string Generate(int length = 5)
         {
+            if (length < 1)
+                throw new ArgumentException("Invalid length value.");
+
             var builder = new StringBuilder();
             for (var i = 0; i < length; i++)
             {
