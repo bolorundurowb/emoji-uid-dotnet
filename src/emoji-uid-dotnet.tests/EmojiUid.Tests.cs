c670d42ea04ee139caba82826016071d375bef4b
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-07-09 22:12:23 +0100

    ft: update the namespace, update language version and use github actions

--- src/emoji-uid-dotnet.tests/EmojiUid.Tests.cs
+++ src/emoji-uid-dotnet.tests/EmojiUid.Tests.cs
@@ -1,31 +1,31 @@
 using System;
-using Xunit;
 using FluentAssertions;
+using Xunit;
 
-namespace emoji_dotnet.tests
+namespace EmojiDotNet.Tests;
+
+public class EmojiUidTests
 {
-    public class EmojiUidTests
+    [Fact]
+    public void ShouldValidateProvidedLength()
     {
-        [Fact]
-        public void ShouldValidateProvidedLength()
-        {
-            Action action = () => EmojiUid.Generate(-2);
-            action.Should().ThrowExactly<ArgumentException>("Invalid length value.");
-        }
+        Action action = () => EmojiUid.Generate(0);
+        action.Should().ThrowExactly<ArgumentException>("Invalid length value.");
+    }
 
-        [Fact]
-        public void ShouldGenerateUidWithDefaultLength()
-        {
-            var id = EmojiUid.Generate();
-            id.Should().NotBeEmpty();
-        }
+    [Fact]
+    public void ShouldGenerateUidWithDefaultLength()
+    {
+        var id = EmojiUid.Generate();
+        id.Should().NotBeEmpty();
+        id.Should().HaveLength(8); // each emoji is made up of two characters
+    }
 
-        [Fact]
-        public void ShouldGenerateUidWithSpecifiedLength()
-        {
-            const int length = 10;
-            var id = EmojiUid.Generate(length);
-            id.Should().NotBeEmpty();
-        }
+    [Fact]
+    public void ShouldGenerateUidWithSpecifiedLength()
+    {
+        const int length = 10;
+        var id = EmojiUid.Generate(length);
+        id.Should().NotBeEmpty();
     }
 }
--- src/emoji-uid-dotnet/EmojiUid.cs
+++ src/emoji-uid-dotnet/EmojiUid.cs
@@ -1,37 +1,35 @@
 ﻿using System;
 using System.Text;
 
-namespace emoji_dotnet
+namespace EmojiDotNet;
+
+public static class EmojiUid
 {
-    public static class EmojiUid
+    // ReSharper disable once InconsistentNaming
+    private static readonly Random _random = new();
+
+    /// <summary>
+    /// Generates a random emoji string of a specified length.
+    /// </summary>
+    /// <param name="length">Minimum length of the generated id.</param>
+    /// <returns>The generated emoji uid.</returns>
+    /// <exception cref="ArgumentException">Thrown when length is less than 1.</exception>
+    public static string Generate(int length = 4)
     {
-        // ReSharper disable once InconsistentNaming
-        private static readonly Random _random = new Random();
+        if (length < 1)
+            throw new ArgumentException("Invalid length value.");
 
-        /// <summary>
-        /// Generates a random emoji string of a specified length.
-        /// </summary>
-        /// <param name="length">Minimum length of the generated id.</param>
-        /// <returns>The generated emoji uid.</returns>
-        /// <exception cref="ArgumentException">Thrown when length is less than 1.</exception>
-        public static string Generate(int length = 3)
-        {
-            if (length < 1)
-                throw new ArgumentException("Invalid length value.");
+        var builder = new StringBuilder();
 
-            var builder = new StringBuilder();
-            for (var i = 0; i < length; i++)
-            {
-                builder.Append(GetEmoji());
-            }
+        for (var i = 0; i < length; i++)
+            builder.Append(GetEmoji());
 
-            return builder.ToString();
-        }
+        return builder.ToString();
+    }
 
-        private static string GetEmoji()
-        {
-            var emojis = Resources.Emojis[_random.Next(Resources.Emojis.Count)];
-            return emojis[_random.Next(emojis.Count)];
-        }
+    private static string GetEmoji()
+    {
+        var emojis = Resources.Emojis[_random.Next(Resources.Emojis.Count)];
+        return emojis[_random.Next(emojis.Count)];
     }
 }
